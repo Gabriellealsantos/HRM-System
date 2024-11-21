736e933285831b73da9d78dde5d93cb9cc0da60a
gabriel <gabriel.lealsantos@hotmail.com>
2024-11-21 11:16:50 -0300

    Projeto iniciado

---
+++ backend/src/test/java/com/inovals/HRMSystem/HrmSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inovals.HRMSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrmSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
